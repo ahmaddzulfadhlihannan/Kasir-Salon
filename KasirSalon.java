0c27a9c811f83ed7b3ca18c64c637699d8287746
ADFH_407 <143983317+ahmaddzulfadhlihannan@users.noreply.github.com>
2023-09-13 10:24:55 +0700

    Update lagi ges buat keterangan

--- KasirSalon.java
+++ KasirSalon.java
@@ -1,12 +1,19 @@
 //Aplikasi Kasir Salon Pertama//
+
+//---import Scanner---//
 import java.util.Scanner;
 public class KasirSalon {
     public static void main(String[] args) {
+
+        //---Deklarasi Scanner---//
         Scanner sc = new Scanner(System.in);
+
+        //---Input dan output---//
         String nmBarang;
         int hrgBarang, jmlBarang;
         double totalHrg, uangDiterima, uangKembalian;
 
+        //---Memasukkan nilai variabel ke scanner---//
         System.out.println("Masukkan nama barang yang dibeli");
         nmBarang = sc.next();
         System.out.println("Masukkan harga barang yang dibeli! ");
@@ -14,13 +21,17 @@ public class KasirSalon {
         System.out.println("Masukkan jumlah barang yang dibeli! ");
         jmlBarang = sc.nextInt();
 
+        //---Hitung jumlah harga---//
         totalHrg = hrgBarang * jmlBarang;
         System.out.println("Total harga barang : " + totalHrg);
 
+        //---Hitung uang kembalian---//
         System.out.println("Masukkan jumlah uang yang diterima ");
         uangDiterima = sc.nextInt();
         uangKembalian = uangDiterima - totalHrg;
+        System.out.println("Uang kembalian" + uangKembalian);
 
+        //---Cetak rincian pembelian---//
         System.out.println("Struk pembelian");
         System.out.println("Nama barang : " + nmBarang);
         System.out.println("Harga barang : " + hrgBarang);
@@ -28,7 +39,7 @@ public class KasirSalon {
         System.out.println("Total harga : " + totalHrg);
         System.out.println("Uang diterima : " + uangDiterima);
         System.out.println("Uang kembalian anda adalah : " + uangKembalian);
-        System.out.println("Terima kasih terlah datang disalon kami");
+        System.out.println("Terima kasih telah datang disalon kami");
 
     }
 }

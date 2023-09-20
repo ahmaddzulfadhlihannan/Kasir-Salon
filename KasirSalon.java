8d3691b0575291f31ec09b0816e1aae756f8c2f1
ADFH_407 <143983317+ahmaddzulfadhlihannan@users.noreply.github.com>
2023-09-20 10:12:16 +0700

    Tamaba

--- KasirSalon.java
+++ KasirSalon.java
@@ -2,43 +2,44 @@
 
 //---import Scanner---//
 import java.util.Scanner;
+
 public class KasirSalon {
     public static void main(String[] args) {
 
-        //---Deklarasi Scanner---//
+        // ---Deklarasi Scanner---//
         Scanner sc = new Scanner(System.in);
 
-        //---Input dan output---//
+        // ---Input dan output---//
         String nmBarang;
         int hrgBarang, jmlBarang;
         double totalHrg, uangDiterima, uangKembalian;
 
-        //---Memasukkan nilai variabel ke scanner---//
-        System.out.println("Masukkan nama barang yang dibeli");
+        // ---Memasukkan nilai variabel ke scanner---//
+        System.out.printf("Masukkan nama barang yang dibeli : ");
         nmBarang = sc.next();
-        System.out.println("Masukkan harga barang yang dibeli! ");
+        System.out.printf("Masukkan harga barang yang dibeli! : ");
         hrgBarang = sc.nextInt();
-        System.out.println("Masukkan jumlah barang yang dibeli! ");
+        System.out.printf("Masukkan jumlah barang yang dibeli! : ");
         jmlBarang = sc.nextInt();
 
-        //---Hitung jumlah harga---//
+        // ---Hitung jumlah harga---//
         totalHrg = hrgBarang * jmlBarang;
-        System.out.println("Total harga barang : " + totalHrg);
+        System.out.printf("Total harga barang : %f", totalHrg);
 
-        //---Hitung uang kembalian---//
-        System.out.println("Masukkan jumlah uang yang diterima ");
+        // ---Hitung uang kembalian---//
+        System.out.printf("\nMasukkan jumlah uang yang diterima : ");
         uangDiterima = sc.nextInt();
         uangKembalian = uangDiterima - totalHrg;
 
-        //---Cetak rincian pembelian---//
-        System.out.println("Struk pembelian");
-        System.out.println("Nama barang : " + nmBarang);
-        System.out.println("Harga barang : " + hrgBarang);
-        System.out.println("Jumlah barang : " + jmlBarang);
-        System.out.println("Total harga : " + totalHrg);
-        System.out.println("Uang diterima : " + uangDiterima);
-        System.out.println("Uang kembalian anda adalah : " + uangKembalian);
-        System.out.println("Terima kasih telah datang disalon kami");
+        // ---Cetak rincian pembelian---//
+        System.out.printf("\nStruk pembelian");
+        System.out.printf("\nNama barang : %s", nmBarang);
+        System.out.printf("\nHarga barang : %d", hrgBarang);
+        System.out.printf("\nJumlah barang : %d", jmlBarang);
+        System.out.printf("\nTotal harga : %f", totalHrg);
+        System.out.printf("\nUang diterima : %f", uangDiterima);
+        System.out.printf("\nUang kembalian anda adalah : %f", uangKembalian);
+        System.out.printf("\nTerima kasih telah datang disalon kami");
 
     }
 }

40b8b8459783aef55e2b7c049ca42904dc787bb2
Hannan <ahmadriski207@gmail.com>
2023-10-16 22:06:14 +0700

    Commit Hannan P7

--- KasirSalon.java
+++ KasirSalon.java
@@ -1,45 +1,123 @@
-//Aplikasi Kasir Salon Pertama//
-
-//---import Scanner---//
 import java.util.Scanner;
 
 public class KasirSalon {
+
     public static void main(String[] args) {
 
-        // ---Deklarasi Scanner---//
         Scanner sc = new Scanner(System.in);
 
-        // ---Input dan output---//
-        String nmBarang;
-        int hrgBarang, jmlBarang;
-        double totalHrg, uangDiterima, uangKembalian;
+        // Variabel
+        int jumlahBarang, jumlahJenis, i = 0;
+        double harga, hargaBarang, diskon = 0, totalHarga = 0, bayar;
+        String namaBarang, membership = "", user;
 
-        // ---Memasukkan nilai variabel ke scanner---//
-        System.out.printf("Masukkan nama barang yang dibeli : ");
-        nmBarang = sc.next();
-        System.out.printf("Masukkan harga barang yang dibeli! : ");
-        hrgBarang = sc.nextInt();
-        System.out.printf("Masukkan jumlah barang yang dibeli! : ");
-        jmlBarang = sc.nextInt();
+        // Deklarasi 1
+        System.out.println("Masukkan jumlah jenis barang");
+        jumlahJenis = sc.nextInt();
 
-        // ---Hitung jumlah harga---//
-        totalHrg = hrgBarang * jmlBarang;
-        System.out.printf("Total harga barang : %f", totalHrg);
+        // Perulangan
 
-        // ---Hitung uang kembalian---//
-        System.out.printf("\nMasukkan jumlah uang yang diterima : ");
-        uangDiterima = sc.nextInt();
-        uangKembalian = uangDiterima - totalHrg;
+        System.out.println("Apakah pelanggan memiliki membership? (y/t)");
+        membership = sc.next();
 
-        // ---Cetak rincian pembelian---//
-        System.out.printf("\nStruk pembelian");
-        System.out.printf("\nNama barang : %s", nmBarang);
-        System.out.printf("\nHarga barang : %d", hrgBarang);
-        System.out.printf("\nJumlah barang : %d", jmlBarang);
-        System.out.printf("\nTotal harga : %f", totalHrg);
-        System.out.printf("\nUang diterima : %f", uangDiterima);
-        System.out.printf("\nUang kembalian anda adalah : %f", uangKembalian);
-        System.out.printf("\nTerima kasih telah datang disalon kami");
+        if (membership.equalsIgnoreCase("y")) {
+            System.out.println("Masukkan user member");
+            user = sc.next();
+            if (user.equalsIgnoreCase("1587")) {
+                while (i < jumlahJenis) {
+
+                    System.out.println("Masukkan nama barang");
+                    namaBarang = sc.next();
+                    System.out.println("Masukkan harga barang");
+                    hargaBarang = sc.nextDouble();
+                    System.out.println("Masukkan jumlah barang");
+                    jumlahBarang = sc.nextInt();
+
+                    harga = hargaBarang * jumlahBarang;
+                    totalHarga += harga;
+                    i++;
+                }
+
+            } else if (user.equalsIgnoreCase("5698")) {
+                while (i < jumlahJenis) {
+
+                    System.out.println("Masukkan nama barang");
+                    namaBarang = sc.next();
+                    System.out.println("Masukkan harga barang");
+                    hargaBarang = sc.nextDouble();
+                    System.out.println("Masukkan jumlah barang");
+                    jumlahBarang = sc.nextInt();
+
+                    harga = hargaBarang * jumlahBarang;
+                    totalHarga += harga;
+                    i++;
+                }
+
+            } else if (user.equalsIgnoreCase("5698")) {
+                while (i < jumlahJenis) {
+
+                    System.out.println("Masukkan nama barang");
+                    namaBarang = sc.next();
+                    System.out.println("Masukkan harga barang");
+                    hargaBarang = sc.nextDouble();
+                    System.out.println("Masukkan jumlah barang");
+                    jumlahBarang = sc.nextInt();
+
+                    harga = hargaBarang * jumlahBarang;
+                    totalHarga += harga;
+                    i++;
+                }
+            } else if (user.equalsIgnoreCase("5984")) {
+                while (i < jumlahJenis) {
+
+                    System.out.println("Masukkan nama barang");
+                    namaBarang = sc.next();
+                    System.out.println("Masukkan harga barang");
+                    hargaBarang = sc.nextDouble();
+                    System.out.println("Masukkan jumlah barang");
+                    jumlahBarang = sc.nextInt();
+
+                    harga = hargaBarang * jumlahBarang;
+                    totalHarga += harga;
+                    i++;
+                }
+            } else {
+                System.out.println("User salah");
+            }
+
+            if (totalHarga >= 300000) {
+                diskon = 0.2;
+            } else if (totalHarga >= 100000) {
+                diskon = 0.15;
+            }
+            bayar = totalHarga - (totalHarga * diskon);
+            System.out.println("Total harga yang perlu dibayar : " + bayar);
+        } else if (membership.equalsIgnoreCase("t")) {
+            while (i < jumlahJenis) {
+
+                System.out.println("Masukkan nama barang");
+                namaBarang = sc.next();
+                System.out.println("Masukkan harga barang");
+                hargaBarang = sc.nextDouble();
+                System.out.println("Masukkan jumlah barang");
+                jumlahBarang = sc.nextInt();
+
+                harga = hargaBarang * jumlahBarang;
+                totalHarga += harga;
+                i++;
+
+            }
+            if (totalHarga >= 350000) {
+                diskon = 0.2;
+            } else if (totalHarga >= 150000) {
+                diskon = 0.15;
+            }
+            bayar = totalHarga - (totalHarga * diskon);
+            System.out.println("Total harga yang perlu dibayar : " + bayar);
+
+        } else {
+            System.out.println("Input membership salah");
+        }
 
         sc.close();
     }

bc696d387fdbefd1868154ddc3a4b9ce70a476d3
ahmaddzulfadhlihannan <ahmadriski207@gmail.com>
2023-10-25 10:33:44 +0700

    Hannan Array Fitur Utama Kasir

--- FiturUtamaPembayaran.java
+++ FiturUtamaPembayaran.java
@@ -5,25 +5,53 @@ public class FiturUtamaPembayaran {
         
         //Scannner
         Scanner sc = new Scanner(System.in);
-        double totalHrg, uangDiterima, uangKembalian;
 
-        //Input total pembelian dan uang yang diterima
-        System.out.print("Total pembelian : ");
-        totalHrg = sc.nextDouble();
-        System.out.print("Uang yang diterima : ");
+        //Masukkan jumlah jenis barang yang dibeli
+        System.out.print("Masukkan jumlah jenis barang yang dibeli : ");
+        int jumlahJenis = sc.nextInt();
+
+        //Variabel
+        String namaBarang[] = new String[jumlahJenis];
+        int jumlahBarang[] = new int[jumlahJenis];
+        double harga[] = new double[jumlahJenis], totalHarga[] = new double[jumlahJenis], totalPembelian = 0, uangDiterima, kembalian;
+
+        //Pembelian
+        for (int i = 0; i < jumlahJenis; i++) {
+            System.out.print("Masukkan nama barang\t" + (i+1) + "\t\t:\t");
+            namaBarang[i] = sc.next();
+            System.out.print("Masukkan harga barang\t" + (i+1) + "\t\t:\t");
+            harga[i] = sc.nextDouble();
+            System.out.print("Masukkan jumlah barang\t" + (i+1) + "\t\t:\t");
+            jumlahBarang[i] = sc.nextInt();
+            totalHarga[i] = (double) harga[i] * jumlahBarang[i];
+            totalPembelian += totalHarga[i];
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Total pembelian\t:\t" + totalPembelian);
+        System.out.print("Uang diterima\t:\t");
         uangDiterima = sc.nextDouble();
+        if (uangDiterima >= totalPembelian) {
+            kembalian = uangDiterima - totalPembelian;
+            System.out.println("Uang kembalian\t:\t" + kembalian);
+        }
+        else {
+            kembalian = totalPembelian - uangDiterima;
+            System.out.println("Uang kurang\t:\t" + kembalian);
+        }
+        System.out.println("-----------------------------------------------------------");
 
-        //Kondisi uang kembalian
-        if (totalHrg<uangDiterima) {
-        uangKembalian=uangDiterima-totalHrg;
-        System.out.print("Uang kembalian yang diterima: " +uangKembalian);
-        } else if (totalHrg==uangDiterima) {
-            System.out.print("Tidak ada kembalian");
-        } else {
-            uangKembalian=totalHrg-uangDiterima;
-            System.out.print("Kembalian kurang: " +uangKembalian); 
+        //Struk pembelian
+        System.out.printf("Nama Barang\tHarga Barang\tJumlah Barang\ttotal Harga\n");
+        for (int i = 0; i < jumlahJenis; i++ ) {
+            System.out.printf((i+1) + "." + namaBarang[i] + "\t\t" + harga[i] + "\t\t" + jumlahBarang[i] + "\t\t" + totalHarga[i] + "\n");
+        
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Total pembelian\t:\t" + totalPembelian);
+        System.out.println("Uang Diterima\t:\t" + uangDiterima);
+        System.out.println("Kembalian\t:\t" + kembalian);
+        System.out.println("----------Terima Kasih Telah Datang Ke Salon Kami----------");
 
         sc.close();
         } 
     }
-}

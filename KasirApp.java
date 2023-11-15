47e4ac179d1913b6871ff214288dba4176456afe
Hannan <ahmadriski207@gmail.com>
2023-11-15 10:39:18 +0700

    P12 ke 2

--- KasirApp.java
+++ KasirApp.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class KasirApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         double penjualanHarian[] = new double[30];
-        int menu;
+        int menu, maxMembership = 12;
+        String[] namaPelanggan = new String[maxMembership];
+        String[] nomorMember = new String[maxMembership];
+        int jumlahPelanggan = 0;
 
         for (int i = 0; i < penjualanHarian.length; i++) {
+            penjualanHarian[i] = 0;
             System.out.println("Aplikasi salon");
             System.out.println("Login karyawan");
             String userKaryawan = sc.next();
@@ -23,6 +29,8 @@ public class KasirApp {
                 System.out.println("Selamat datang di aplikasi salon");
                 System.out.println("Menu");
                 System.out.println("1. Fitur utama pembayaran dan struk");
+                System.out.println("2. Pendaftaran membership");
+                System.out.println("3. Daftar pelanggan membership");
                 System.out.println("0. Keluar");
                 System.out.println("Pilih menu");
                 menu = sc.nextInt();
@@ -51,6 +59,7 @@ public class KasirApp {
                             jumlah[j] = sc.nextInt();
                             harga[j] = hargaJenisPelayanan[nomorItem[j] - 1] * jumlah[j];
                             totalHarga += harga[j];
+                            penjualanHarian[i] += totalHarga;
                             j++;
                         } while (nomorItem[j - 1] != 0);
 
@@ -93,6 +102,46 @@ public class KasirApp {
                         }
                         break;
 
+                    case 2:
+                        // Pendaftaran pelanggan membership
+                        while (jumlahPelanggan < maxMembership) {
+                            System.out.println("Pendaftaran Pelanggan Membership");
+                            System.out.print("Nama Pelanggan: ");
+                            String nama = sc.next();
+                
+                            // Memastikan nama pelanggan unik
+                            if (isNamaPelangganUnique(nama, namaPelanggan, jumlahPelanggan)) {
+                                // Generate nomor member secara acak
+                                String nomorMemberBaru = generateNomorMember();
+                                
+                                namaPelanggan[jumlahPelanggan] = nama;
+                                nomorMember[jumlahPelanggan] = nomorMemberBaru;
+                                jumlahPelanggan++;
+                
+                                System.out.println("Pendaftaran berhasil!\n");
+                            } else {
+                                System.out.println("Nama pelanggan sudah terdaftar. Mohon coba lagi.\n");
+                            }
+                
+                            System.out.print("Apakah ingin mendaftar pelanggan lagi? (y/n): ");
+                            String jawaban = sc.nextLine();
+                
+                            if (!jawaban.equalsIgnoreCase("y")) {
+                                break;
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("\nData Pelanggan Membership:");
+                        for (int l = 0; l < jumlahPelanggan; l++) {
+                            System.out.println("Pelanggan ke-" + (l + 1));
+                            System.out.println("Nama: " + namaPelanggan[l]);
+                            System.out.println("Nomor Member: " + nomorMember[l]);
+                            System.out.println("--------------------");
+                        }
+                        break;
+
                     case 0:
                         // Keluar dari aplikasi
                         System.exit(0);
@@ -110,5 +159,41 @@ public class KasirApp {
         // Ganti dengan logika login yang sesuai, misalnya dari database
         return username.equals("karyawan") && password.equals("password");
 
+    }
+
+    // Fungsi untuk memeriksa apakah nama pelanggan sudah terdaftar
+    private static boolean isNamaPelangganUnique(String nama, String[] namaPelanggan, int jumlahPelanggan) {
+        for (int i = 0; i < jumlahPelanggan; i++) {
+            if (nama.equals(namaPelanggan[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+     
+
+
+    // Fungsi untuk memeriksa apakah nomor member sudah terdaftar
+    /*private static boolean isNomorMemberUnique(String nomorMember, String[] nomorMemberArray, int jumlahPelanggan) {
+        for (int i = 0; i < jumlahPelanggan; i++) {
+            if (nomorMember.equals(nomorMemberArray[i])) {
+                return false;
+            }
+        }
+        return true;
+    }*/
+
+    // Fungsi untuk generate nomor member secara acak
+    private static String generateNomorMember() {
+        Random acak = new Random();
+        String karakter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder nomorMemberBaru = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            int index = acak.nextInt(karakter.length());
+            nomorMemberBaru.append(karakter.charAt(index));
+        }
+
+        return nomorMemberBaru.toString();
     }
 }

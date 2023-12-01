38f4eb562911e642bbad4ccd32f429e583d206af
NurAyuL27 <nurayu2718@gmail.com>
2023-12-02 00:30:18 +0700

    update ayu

--- FiturMembership.java
+++ FiturMembership.java
@@ -1,39 +1,54 @@
 import java.util.Scanner;
-
 public class FiturMembership {
     public static void main(String[] args) {
-        java.util.Scanner sc = new Scanner(System.in);
-        String member;
-        int totHarga, hargaAkhir;
-        double diskon = 0;
+        Scanner sc = new Scanner(System.in);
+        int jumlahTransaksi;
 
-        System.out.print("membership atau bukan          : ");
-        member = sc.nextLine();
-        System.out.print("Masukkan total harga pembelian : ");
-        totHarga = sc.nextInt();
+        System.out.print("Masukkan jumlah transaksi: ");
+        jumlahTransaksi = sc.nextInt();
 
-        if (member.equalsIgnoreCase("membership")) {
-            if (totHarga >= 300000) {
-                diskon = 0.2;
-            } else if (totHarga >= 100000) {
-                    diskon = 0.15;
-                } else {
-                    diskon = 0;
+        String[] member = new String[jumlahTransaksi];
+        int[] totHarga = new int[jumlahTransaksi];
+        double[] diskon = new double[jumlahTransaksi];
+        int[] hargaAkhir = new int[jumlahTransaksi];
+
+        for (int i = 0; i < jumlahTransaksi; i++) {
+            System.out.print("Membership atau bukan (transaksi ke-" + (i + 1) + "): ");
+            member[i] = sc.next();
+
+            if (member[i].equalsIgnoreCase("membership") || member[i].equalsIgnoreCase("bukan")) {
+                System.out.print("Masukkan total harga pembelian (transaksi ke-" + (i + 1) + "): ");
+                totHarga[i] = sc.nextInt();
+            } else {
+                System.out.println("Jenis keanggotaan tidak valid (transaksi ke-" + (i + 1) + ")");
+                continue;
             }
-            hargaAkhir = (int) (totHarga - (totHarga * diskon));
-            System.out.println("Total harga yang perlu dibayar : " + hargaAkhir);
-        } else if (member.equalsIgnoreCase("bukan")) {
-            if (totHarga >= 150000) {
-                diskon = 0.02;
-            } else if (totHarga >= 80000) {
-                    diskon = 0.005;
+        }
+
+        for (int i = 0; i < jumlahTransaksi; i++) {
+            if (member[i].equalsIgnoreCase("membership")) {
+                if (totHarga[i] >= 300000) {
+                    diskon[i] = 0.2;
+                } else if (totHarga[i] >= 100000) {
+                    diskon[i] = 0.15;
                 } else {
-                    diskon = 0;
+                    diskon[i] = 0;
+                }
+            } else if (member[i].equalsIgnoreCase("bukan")) {
+                if (totHarga[i] >= 150000) {
+                    diskon[i] = 0.02;
+                } else if (totHarga[i] >= 80000) {
+                    diskon[i] = 0.005;
+                } else {
+                    diskon[i] = 0;
+                }
+            } else {
+                System.out.println("Bukan pelanggan (transaksi ke-" + (i + 1) + ")");
+                continue;
             }
-        hargaAkhir = (int) (totHarga - (totHarga * diskon));
-        System.out.println("Total harga yang perlu dibayar : " + hargaAkhir);
-        } else {
-            System.out.println("Bukan pelanggan");
+
+            hargaAkhir[i] = (int) (totHarga[i] - (totHarga[i] * diskon[i]));
+            System.out.println("Total harga yang perlu dibayar (transaksi ke-" + (i + 1) + "): " + hargaAkhir[i]);
         }
     }
 }

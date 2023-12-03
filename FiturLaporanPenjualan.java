c89ca6222fded44df3423ec6ac4ae646ad768e07
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2023-12-03 07:56:44 +0700

    ubah

--- FiturLaporanPenjualan.java
+++ FiturLaporanPenjualan.java
@@ -3,39 +3,44 @@ import java.util.Scanner;
 public class FiturLaporanPenjualan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] pendapatanPotong = new int[31];
-        int[] pendapatanRias = new int[31];
-        int tanggal, pendapatan, layanan;
-        boolean masukkanLagi = true;
+        String jenisLayanan;
+        double hasildiDpt;
 
-        while (masukkanLagiLagi) {
-            System.out.print("Masukkan tanggal (1-31): ");
-            tanggal = sc.nextInt();
-            System.out.print("Jenis layanan (1 untuk potong, 2 untuk rias): ");
-            layanan = sc.nextInt();
-            System.out.print("Masukkan pendapatan: ");
-            pendapatan = sc.nextInt();
-    
+        double[][] penjualan = new double[31][2];
 
-            if (layanan == 1) {
-                pendapatanPotong[tanggal - 1] += pendapatan;
-            } else if (layanan == 2) {
-                pendapatanRias[tanggal - 1] += pendapatan;
+        while (true) {
+            System.out.print("Masukkan tanggal (1-31) : ");
+            int tanggal = sc.nextInt();
+            sc.nextLine();
+
+            if (tanggal < 1 || tanggal > 31) {
+                System.out.println("Tanggal tidak valid. Harap masukkan tanggal antara 1 hingga 31.");
+                continue;
             }
 
-            System.out.print("Apakah ingin memasukkan pendapatan lagi? (true/false): ");
-            masukkanLagi = sc.nextBoolean();
-        }
+            System.out.println("Jenis layanan Potong atau Rias");
+            System.out.print("Masukkan jenis layanan (Potong/Rias) : ");
+            jenisLayanan = sc.nextLine();
 
-        // Proses rekap pada hari terakhir di bulan
-        int totalPendapatanPotong = 0;
-        int totalPendapatanRias = 0;
-        for (int i = 0; i < 31; i++) {
-            totalPendapatanPotong += pendapatanPotong[i];
-            totalPendapatanRias += pendapatanRias[i];
-        }
+            if (!jenisLayanan.equalsIgnoreCase("Potong") && !jenisLayanan.equalsIgnoreCase("Rias")) {
+                System.out.println("Jenis layanan tidak valid. Harap masukkan Potong atau Rias.");
+                continue;
+            }
 
-        System.out.println("Total pendapatan potong: " + totalPendapatanPotong);
-        System.out.println("Total pendapatan rias: " + totalPendapatanRias);
+            System.out.print("Masukkan pendapatan hari ini : ");
+            hasildiDpt = sc.nextDouble();
+            sc.nextLine();
+
+            if (jenisLayanan.equals("Potong")) {
+                penjualan[tanggal - 1][0] = hasildiDpt;
+            } else {
+                penjualan[tanggal - 1][1] = hasildiDpt;
+            }
+            System.out.print("Apakah Anda ingin memasukkan data? (y/n) : ");
+            String lanjut = sc.nextLine();
+            if (lanjut.equalsIgnoreCase("n")) {
+                break;  
+            }
+        }
     }
 }
